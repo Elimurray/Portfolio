2c4b389bdf5dacf6233a03a9213cdc2ba07848d9
Elimurray <eli.murr04@gmail.com>
2024-10-13 18:09:51 +1300

    nav highlight

--- js/func.js
+++ js/func.js
@@ -17,3 +17,25 @@ function smoothScrolling() {
 smoothScrolling();
 
 
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('.nav-link');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                if (link.getAttribute('href') === `#${entry.target.id}`) {
+                    link.classList.add('active');
+                }
+            });
+        }
+    });
+}, { threshold: 1 }); // Adjust threshold as needed
+
+sections.forEach(section => {
+    observer.observe(section);
+});
+
+
+
