b66b33aa6096f6feac1d5b5b334b52156f8d27e4
Elimurray <eli.murr04@gmail.com>
2025-09-08 21:53:28 +1200

    minor changes to style

--- js/func.js
+++ js/func.js
@@ -1,41 +1,40 @@
 function smoothScrolling() {
-    document.querySelectorAll('nav a').forEach(anchor => {
-        anchor.addEventListener('click', function(e) {
-            e.preventDefault();
+  document.querySelectorAll("nav a").forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
 
-            const targetId = this.getAttribute('href');
-            const targetElement = document.querySelector(targetId);
+      const targetId = this.getAttribute("href");
+      const targetElement = document.querySelector(targetId);
 
-            targetElement.scrollIntoView({
-                behavior: 'smooth',
-                block: 'start'
-            });
-        });
+      targetElement.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
     });
+  });
 }
 
 smoothScrolling();
 
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
 
-const sections = document.querySelectorAll('section');
-const navLinks = document.querySelectorAll('.nav-link');
-
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            navLinks.forEach(link => {
-                link.classList.remove('active');
-                if (link.getAttribute('href') === `#${entry.target.id}`) {
-                    link.classList.add('active');
-                }
-            });
-        }
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        navLinks.forEach((link) => {
+          link.classList.remove("active");
+          if (link.getAttribute("href") === `#${entry.target.id}`) {
+            link.classList.add("active");
+          }
+        });
+      }
     });
-}, { threshold: 0.8 }); // Adjust threshold as needed
+  },
+  { threshold: 0.8 }
+); // Adjust threshold as needed
 
-sections.forEach(section => {
-    observer.observe(section);
+sections.forEach((section) => {
+  observer.observe(section);
 });
-
-
-
